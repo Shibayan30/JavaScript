fdff4376647ab7da8b80f193f60bc016d6319184
Shibayan30 <shibayanroy46@gmail.com>
2025-03-18 08:36:20 +0000

    container config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=300305
+let accountEmail="shibayan30@gmail.com"
+var accountPassword="123456"
+accountCity="Kolkata"
+console.log(accountId,accountEmail,accountPassword,accountCity)
+accountEmail="ab@ab.com"
+accountPassword="654321"
+accountCity="Chennai"
+console.table([accountId,accountEmail,accountPassword,accountCity])
